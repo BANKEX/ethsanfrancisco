05dd3099d0bf11098018a24ee87af6914d1ad345
Kirill <krboktv@gmail.com>
2018-10-06 10:42:52 -0700

    Add full api, bot functions and frontend

---
+++ telegram/app.js
@@ -0,0 +1,26 @@
+const Telegraf = require('telegraf');
+const session = require('telegraf/session');
+const Handlers = require('./handlers/handlers');
+const Text = require('./text');
+const Stage = require("telegraf/stage");
+
+const stage = new Stage();
+
+// require('dotenv').config({path: "../.env"});
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.use(session({ ttl: 10 }));
+bot.use(stage.middleware());
+
+bot.start((ctx) => Handlers.start(ctx));
+
+bot.hears(Text.keyboard.start.button["0"], (ctx) => Handlers.goToAccount(ctx));
+bot.hears(Text.keyboard.start.button["1"], (ctx) => ctx.scene.enter("sendTransaction"));
+bot.hears(Text.keyboard.account.button["0"], (ctx) => Handlers.getAddresses(ctx));
+bot.hears(Text.back, (ctx) => Handlers.back(ctx));
+
+stage.register(Handlers.sendTransaction);
+
+bot.startPolling();
+
