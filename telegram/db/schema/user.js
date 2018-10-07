c1f08b65e8c2df4b48ba140657535028ddc3f7e8
Kirill <krboktv@gmail.com>
2018-10-07 13:43:41 -0700

    Hardcode token addresses

--- telegram/db/schema/user.js
+++ telegram/db/schema/user.js
@@ -21,7 +21,13 @@ const User = new Schema({
         default: ''
     },
     tokenAddresses: {
-      type: Array
+      type: Array,
+      default: [
+        "0x55080ac40700BdE5725D8a87f48a01e192F660AF",
+        "0x659c4206b2ee8CC00af837CddA132eb30fA58df8",
+        "0x6b1224743CC0E5994d1B35f2b90A1A12a51044aC",
+        "0xa8787F4faB0467B80d2F57722b1Bc8DDE707630E"
+      ]
     },
     registrationDate: {
         type: Date,
